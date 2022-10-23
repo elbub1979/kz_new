dfc7a4ab2ad380b85a6cc90e05d4d7317846609c
elbub <ilya.leuh09@gmail.com>
2022-10-23 15:56:58 +0300

    added phonebooksController#fill_in

--- app/controllers/phonebooks_controller.rb
+++ app/controllers/phonebooks_controller.rb
@@ -2,4 +2,21 @@ class PhonebooksController < ApplicationController
   def index
     @users = User.order(lname: :desc)
   end
+
+  def create; end
+
+  def fill_in
+    all_contacts = Kerio::KerioConnect.get_all_contacts
+
+    all_contacts.each do |contact|
+      number = contact[:number]
+      fname = contact[:fname]
+      lname = contact[:lname]
+      pname = contact[:pname]
+
+      user = User.find_or_create_by(fname: fname, lname: lname, pname: pname)
+
+      InternalPhone.find_or_create_by(number, user: user)
+    end
+  end
 end
---
+++ lib/Kerio.rb
@@ -0,0 +1,9 @@
+module Kerio
+  class KerioConnect
+    class << self
+      def get_all_contacts
+
+      end
+    end
+  end
+end
