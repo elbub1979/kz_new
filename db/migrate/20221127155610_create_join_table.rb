2806bbe23520338aed5d8955524dd54f9dcc9197
elbub <ilya.leuh09@gmail.com>
2022-11-28 00:18:42 +0300

    add & remove join table between user & department

--- db/migrate/20221127155610_create_join_table.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTable < ActiveRecord::Migration[7.0]
-  def change
-    create_join_table :users, :departments do |t|
-      t.index :user_id
-      t.index :department_id
-    end
-  end
-end
