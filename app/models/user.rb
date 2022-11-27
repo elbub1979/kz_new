4b43ca1eeafcf7e780dbf977bbe588d45b703e60
elbub <ilya.leuh09@gmail.com>
2022-11-28 00:26:09 +0300

    refactoring references in model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  has_many :internal_phones
-  has_many :dsp_data_carriers, class_name: 'Dsp::DataCarrier'
-  has_and_belongs_to_many :departments
+  # has_many :internal_phones
+  # has_many :dsp_data_carriers, class_name: 'Dsp::DataCarrier'
+
+  has_many :employees
+  has_many :departments, through: :employees
 
   validates :lname, presence: true
 
