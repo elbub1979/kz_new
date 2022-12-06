ce652fbfd13fe60c26ec97a0eb1d535fe38bd308
elbub <ilya.leuh09@gmail.com>
2022-12-06 22:07:47 +0300

    added lib/active_directory class; custom read_from_ad action to registration controller; custom route read_from_ad;

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
 
+  devise_scope :user do
+    get 'read_from_ad', to: 'users/registrations#read_from_ad' #, as: 'read_from_ad'
+  end
+
   resources :phonebooks do
-    get 'fill_in', on: :collection
     collection do
       get 'list'
-    end
+      get 'fill_in'end
   end
 
   namespace :dsp do
---
+++ lib/active_directory.rb
@@ -0,0 +1,49 @@
+require 'net-ldap'
+
+class ActiveDirectory
+  def initialize(*args)
+    @type = args[:type]
+    @host = Rails.application.credentials.active_directory.host
+    @port = Rails.application.credentials.active_directory.port
+    @dc1 = Rails.application.credentials.active_directory.dc1
+    @dc2 = Rails.application.credentials.active_directory.dc2
+    @ou = Rails.application.credentials.active_directory.ou
+    @admin_login = Rails.application.credentials.active_directory.admin_login
+    @admin_password = Rails.application.credentials.active_directory.admin_password
+  end
+
+  def read
+    ldap = Net::LDAP.new(host: @host, port: @port)
+    treebase = "OU=#{@type}, OU=#{@ou}, DC=#{@dc2}, DC=#{@dc1}"
+
+    ldap.auth username, password
+
+    ldap.search(base: treebase)[0..20].map { |entry| entry['name'][0].split(' ') }
+
+  end
+end
+
+# puts @ldap.bind ? true : false
+
+# @filter = ~ Net::LDAP::Filter.eq('CN', 'ExchangeActiveSyncDevices') & Net::LDAP::Filter.eq('CN', '*')
+# @filter = ~ Net::LDAP::Filter.eq('CN', 'ExchangeActiveSyncDevices')
+# @filter = ~ Net::LDAP::Filter.eq('objectClass', 'ExchangeActiveSyncDevices')
+# @filter = Net::LDAP::Filter.eq('CN', '*')
+
+#:objectclass
+# CN=ExchangeActiveSyncDevices,CN=Мартынов Павел Сергеевич,OU=USERS,OU=REDSTAR,DC=redstar,DC=ru
+
+# @ldap.search(base: @treebase)[20].each { |key, value| p "#{key}: #{value}" }
+
+# puts @ldap.search(base: @treebase)[20]['name']
+
+# @groups = @ldap.search(bse: @treebase).map do |group|
+# name = group['name'].first.to_s
+# Group.new(name: name)
+# end
+
+# @ldap.search(base: @treebase) do |entry|
+  # p "DN: #{entry.dn}"
+# end
+
+# p @ldap.search(base: @treebase)[1]
